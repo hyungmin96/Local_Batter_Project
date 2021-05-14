4ae831c160aae9d9fdc9b3ba8c329f6fb64b0e62
bana_na21 <bana_na21@naver.com>
2021-05-15 02:14:37 +0900

    db 저장오류

--- src/main/java/com/imageupload/example/vo/FileVo.java
+++ src/main/java/com/imageupload/example/vo/FileVo.java
@@ -1,11 +1,16 @@
 package com.imageupload.example.vo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,13 +18,24 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class FileVo {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column
     private String fileName;
+
+    @Column
     private String filePath;
+    
+    @Column
     private long fileSize;
+
+    // @ManyToOne(targetEntity = articleVo.class, fetch = FetchType.EAGER)
+    // @JoinColumn(name = "boardId")
+    // private articleVo boardId;
 
 }
