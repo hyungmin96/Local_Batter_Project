874156761dea9cb0e50013d93ecd45f6a71466f5
bana_na21 <bana_na21@naver.com>
2021-05-15 15:44:25 +0900

    이미지 업로드 및 게시글작성 기능 구현

--- src/main/java/com/imageupload/example/vo/FileVo.java
+++ src/main/java/com/imageupload/example/vo/FileVo.java
@@ -1,41 +1,37 @@
-package com.imageupload.example.vo;
+package com.imageupload.example.Vo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class FileVo {
+@Entity
+public class fileVo {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column
-    private String fileName;
-
-    @Column
-    private String filePath;
+    private String tempName;
     
     @Column
+    private String filePath;
+
+    @Column
+    private String originName;
+
+    @Column
     private long fileSize;
-
-    // @ManyToOne(targetEntity = articleVo.class, fetch = FetchType.EAGER)
-    // @JoinColumn(name = "boardId")
-    // private articleVo boardId;
-
+    
 }
